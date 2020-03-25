1e1b4f0f8379435029cf6a05aee61c96d04ffca4
Kien Nguyen <ryan@exgo.vn>
2020-03-25 21:16:26 +0700

    demo sharepreferences

--- app/src/main/java/com/code4func/helloandroid/sharepref/SharePreferences.kt
+++ app/src/main/java/com/code4func/helloandroid/sharepref/SharePreferences.kt
@@ -2,31 +2,26 @@ package com.code4func.helloandroid.sharepref
 
 import android.content.SharedPreferences
 
-inline fun <reified T> SharedPreferences.get(key: String, defaultValue: T): T {
+inline fun <reified T> SharedPreferences.get(key: String, defaultValue: T) = with(this) {
     when(T::class) {
-        Boolean::class -> return this.getBoolean(key, defaultValue as Boolean) as T
-        Float::class -> return this.getFloat(key, defaultValue as Float) as T
-        Int::class -> return this.getInt(key, defaultValue as Int) as T
-        Long::class -> return this.getLong(key, defaultValue as Long) as T
-        String::class -> return this.getString(key, defaultValue as String) as T
+        Boolean::class -> getBoolean(key, defaultValue as Boolean) as T
+        Float::class -> getFloat(key, defaultValue as Float) as T
+        Int::class -> getInt(key, defaultValue as Int) as T
+        Long::class -> getLong(key, defaultValue as Long) as T
+        String::class -> getString(key, defaultValue as String) as T
         else -> {
             null
         }
     }
-
-    return defaultValue
 }
 
-inline fun <reified T> SharedPreferences.put(key: String, value: T) {
-    val editor = this.edit()
-
+inline fun <reified T> SharedPreferences.put(key: String, value: T) = with(this.edit()) {
     when(T::class) {
-        Boolean::class -> editor.putBoolean(key, value as Boolean)
-        Float::class -> editor.putFloat(key, value as Float)
-        Int::class -> editor.putInt(key, value as Int)
-        Long::class -> editor.putLong(key, value as Long)
-        String::class -> editor.putString(key, value as String)
+        Boolean::class -> putBoolean(key, value as Boolean)
+        Float::class -> putFloat(key, value as Float)
+        Int::class -> putInt(key, value as Int)
+        Long::class -> putLong(key, value as Long)
+        String::class -> putString(key, value as String)
     }
-
-    editor.apply()
+    commit()
 }
